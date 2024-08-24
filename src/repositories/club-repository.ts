a6be3640805dcdbd41db811923bfb1afa987d2c8
FranciscoBraga <franciscobraga.fob@gmail>
2024-08-24 11:48:58 -0400

    lendo arquivo json e cors

--- src/repositories/club-repository.ts
+++ src/repositories/club-repository.ts
@@ -1,17 +1,18 @@
 import {ClubModel} from "../model/club-model"
-
+import fs from "fs/promises";
 
 
 const dataBase: ClubModel[]=[
     {
         id:1,
         name:"Fluminense"
-}
+ }
 ]
 
 export const findAllClubs = async():Promise<ClubModel[]>=>{
-    console.log(dataBase)
- return dataBase
+    const data =  await fs.readFile("./src/data/database.json","utf-8")
+    const clubs : ClubModel[] = JSON.parse(data)
+ return clubs
 }
 
 
